e9438d6212530e91e77090b1d38e265ed010dd01
Ezgi Kuyucu <131214399+G-EzgiKY@users.noreply.github.com>
2023-12-22 04:14:13 +0300

    Create Button.jsx

---
+++ Button.jsx
@@ -0,0 +1,11 @@
+export default function Button(){
+    return(
+
+        <button  class="button active" href="https://www.linkedin.com/in/ezgi-kuyucu-031593223/">LinkdIn Hesabına Git </button>
+    )
+
+
+
+}
+
+//export default Button
